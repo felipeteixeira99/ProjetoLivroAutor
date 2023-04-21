6b0d050b636b995c2cab58ac2e9060dfaa40a2a1
Felipe Teixeira <felipefggw@gmail.com>
2023-04-21 14:03:26 -0300

    Criacao das classes Livro e Testar Livro

---
+++ LivroAutor/src/TestarLivro.java
@@ -0,0 +1,34 @@
+
+public class TestarLivro {
+	private String titulo;
+	private Autor autor;
+	private String editora;
+	private int anoDePublicacao;
+
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public int getAnoDePublicacao() {
+		return anoDePublicacao;
+	}
+	public void setAnoDePublicacao(int anoDePublicacao) {
+		this.anoDePublicacao = anoDePublicacao;
+	}
+}
+
+
